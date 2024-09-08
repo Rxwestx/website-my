6c330dbd9131a7c908885b395c41293be6043690
Rxwestx <wexriexst13@gmail.com>
2024-09-08 17:29:19 +0900

    no message

---
+++ src/data/mainData.js
@@ -0,0 +1,15 @@
+export const mainData = {
+  common: {
+    siteName: "SiiD Webサイト",
+  },
+  index: {
+    title: "Index Page",
+    desc: "This is the index page.",
+    keywords: "index, home, main",
+  },
+  about: {
+    title: "About Page",
+    desc: "This is the about page.",
+    keywords: "about, us",
+  },
+};
